c617c550b49ed1ec7beea6b40d21c27f9615cac8
Carise Fernandez <carise@gmail.com>
2021-07-23 14:23:46 -0700

    Render aliens

--- go_invaders.go
+++ go_invaders.go
@@ -20,7 +20,7 @@ type Battlefield struct {
 func (*SpaceInvaderScene) Type() string { return "space invaders" }
 
 func (*SpaceInvaderScene) Preload() {
-	engo.Files.Load("sprites/alien1.png", "sprites/small_turret.png")
+	engo.Files.Load("sprites/small_alien1.png", "sprites/small_turret.png")
 
 	engo.Input.RegisterButton("shoot", engo.KeySpace)
 	engo.Input.RegisterAxis("updown", engo.AxisKeyPair{Min: engo.KeyW, Max: engo.KeyS})
@@ -28,12 +28,17 @@ func (*SpaceInvaderScene) Preload() {
 }
 
 func (*SpaceInvaderScene) Setup(u engo.Updater) {
-
 	world, _ := u.(*ecs.World)
-	world.AddSystem(&common.RenderSystem{})
-	world.AddSystem(&systems.ControlSystem{})
 	world.AddSystem(&systems.AlienSystem{})
+	world.AddSystem(&systems.ControlSystem{})
+	world.AddSystem(&common.RenderSystem{})
 
+	AddTurret(world)
+	AddAliens(world, 48)
+
+}
+
+func AddTurret(world *ecs.World) {
 	turret := systems.Turret{BasicEntity: ecs.NewBasic()}
 	turret.SpaceComponent = common.SpaceComponent{
 		Position: engo.Point{X: 200, Y: 350},
@@ -49,13 +54,55 @@ func (*SpaceInvaderScene) Setup(u engo.Updater) {
 		Drawable: turretTexture,
 		Scale:    engo.Point{X: 1, Y: 1},
 	}
-
 	for _, system := range world.Systems() {
 		switch sys := system.(type) {
 		case *common.RenderSystem:
+			log.Println("Render turret", turret.Position)
 			sys.Add(&turret.BasicEntity, &turret.RenderComponent, &turret.SpaceComponent)
 		case *systems.ControlSystem:
 			sys.Add(&turret.BasicEntity, &turret.SpaceComponent)
+		}
+	}
+}
+
+func AddAliens(world *ecs.World, numAliens int) {
+	currentX := float32(60.0)
+	currentY := float32(100.0)
+
+	alien1Texture, err := common.LoadedSprite("sprites/small_alien1.png")
+	log.Println("Load alien1 texture")
+	if err != nil {
+		log.Println("Unable to load alien1 texture", err)
+	}
+
+	for i := 0; i < numAliens; i++ {
+		alien := systems.Alien{BasicEntity: ecs.NewBasic()}
+		if i > 0 {
+			if i%12 == 0 {
+				currentX = float32(60.0)
+				currentY = currentY + 40
+			} else {
+				currentX += 40
+			}
+		}
+		alien.SpaceComponent = common.SpaceComponent{
+			Position: engo.Point{X: currentX, Y: currentY},
+			Width:    30,
+			Height:   30,
+		}
+		alien.RenderComponent = common.RenderComponent{
+			Drawable: alien1Texture,
+			Scale:    engo.Point{X: 1, Y: 1},
+		}
+
+		for _, system := range world.Systems() {
+			switch sys := system.(type) {
+			case *common.RenderSystem:
+				log.Println("Render alien", alien.Position)
+				sys.Add(&alien.BasicEntity, &alien.RenderComponent, &alien.SpaceComponent)
+			case *systems.AlienSystem:
+				sys.Add(alien)
+			}
 		}
 	}
 }
--- systems/alien_system.go
+++ systems/alien_system.go
@@ -1,21 +1,25 @@
 package systems
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/EngoEngine/ecs"
 	"github.com/EngoEngine/engo/common"
-	"github.com/Noofbiz/engoBox2dSystem"
 )
 
-type AlienSystem struct{}
+type AlienSystem struct {
+	entities []Alien
+}
 
 type Alien struct {
 	ecs.BasicEntity
 
 	common.RenderComponent
 	common.SpaceComponent
-	engoBox2dSystem.Box2dComponent
+}
+
+func (a *AlienSystem) Add(alien Alien) {
+	a.entities = append(a.entities, alien)
 }
 
 func (*AlienSystem) Remove(ecs.BasicEntity) {}
@@ -23,5 +27,5 @@ func (*AlienSystem) Remove(ecs.BasicEntity) {}
 func (*AlienSystem) Update(dt float32) {}
 
 func (*AlienSystem) New(*ecs.World) {
-	fmt.Println("new AlienSystem added to scene")
+	log.Println("new AlienSystem added to scene")
 }
